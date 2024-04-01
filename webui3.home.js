a82333b1b572e2661572e365823d94bc7110d421
owen silberman <95719237+owenlikecoding@users.noreply.github.com>
2024-04-01 18:26:56 -0400

    Add files via upload

---
+++ webui3.home.js
@@ -0,0 +1,4 @@
+function othersite() {
+            // Replace 'https://www.othersite.com' with the URL of the site you want to open
+            window.open('https://twitter.com/Gamma1Dev', '_blank');
+}
